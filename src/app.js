fffe306fc7af42f084758ee9c9c3f3c46cb0c73f
BreAnna Baker <124074031+brennybaker@users.noreply.github.com>
2023-03-27 17:51:14 +0000

    temp changes 2

--- src/app.js
+++ src/app.js
@@ -7,7 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   //write your code here
-  document.querySelector("card").classList.add;
+  document.querySelector("card").classList.add("club");
+  document.querySelector("card").classList.add("diamond");
+  document.querySelector("card").classList.add("heart");
+  document.querySelector("card").classList.add("spade");
+};
 
-  console.log("HEY!");
+let generateRanNum = () => {
+  let numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let numbersIndex = Math.floor(Math.random() * numbers.length);
+  return numbers[numbersIndex];
+};
+
+let generateRanSuit = () => {
+  let suit = ["club", "diamond", "heart", "spade"];
+  let suitIndex = Math.floor(Math.random() * suit.length);
+  return suit[suitIndex];
 };
